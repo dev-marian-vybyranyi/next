ec12e3c2b9e22ce0eb3c257febe8e77e2cd02fcc
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2025-12-22 12:57:47 +0200

    Implement new issue submission form with react-hook-form and axios.

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,19 +1,45 @@
 "use client";
 import { Button, TextField } from "@radix-ui/themes";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
+import { Controller, useForm } from "react-hook-form";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
 });
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Title"></TextField.Root>
-      <SimpleMDE placeholder="Description" />
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
+      <TextField.Root
+        placeholder="Title"
+        {...register("title")}
+      ></TextField.Root>
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
