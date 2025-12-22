b677ba1318df9e5ca1feb40805b44f4cda75f412
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2025-12-22 12:46:18 +0200

    integrate SimpleMDE for rich text issue description input.

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,17 @@
-import { TextArea, TextField, Button } from "@radix-ui/themes";
+"use client";
+import { Button, TextField } from "@radix-ui/themes";
+import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 const NewIssuePage = () => {
   return (
     <div className="max-w-xl space-y-3">
       <TextField.Root placeholder="Title"></TextField.Root>
-      <TextArea placeholder="Description" />
+      <SimpleMDE placeholder="Description" />
       <Button>Submit New Issue</Button>
     </div>
   );
