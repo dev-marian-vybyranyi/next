1a0bfcf885747dab6e80c1e3afcce6b74839bbcd
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2025-12-23 22:21:32 +0200

    Filter Issues

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -2,6 +2,7 @@
 
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const statuses: { label: string; value?: Status }[] = [
   { label: "All" },
@@ -11,12 +12,24 @@ const statuses: { label: string; value?: Status }[] = [
 ];
 
 const IssueStatusFilter = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
   return (
-    <Select.Root>
+    <Select.Root
+      defaultValue={searchParams.get("status") || "ALL"}
+      onValueChange={(status) => {
+        const query = status === "ALL" ? "" : `?status=${status}`;
+        router.push("/issues/list" + query);
+      }}
+    >
       <Select.Trigger placeholder="Filter by status..." />
       <Select.Content>
         {statuses.map((status) => (
-          <Select.Item key={status.value || "ALL"} value={status.value || "ALL"}>
+          <Select.Item
+            key={status.value || "ALL"}
+            value={status.value || "ALL"}
+          >
             {status.label}
           </Select.Item>
         ))}
