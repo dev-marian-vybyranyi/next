755d01f62a0298bb7ec70e93b25d309ad29df1b3
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2025-12-23 13:58:25 +0200

    add Google authentication provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,11 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+export const { handlers, auth, signIn, signOut } = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        })
+    ],
 })
