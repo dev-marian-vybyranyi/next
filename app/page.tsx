c0d01da12113cba31f359a8c9f37dc9805ddc213
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2025-12-24 08:32:39 +0200

    Return previous changes to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,3 @@
-import Pagination from "./components/Pagination";
-
-interface Props {
-  searchParams: { page: string };
-}
-export default async function Home({ searchParams }: Props) {
-  const params = await searchParams;
-  const page = parseInt(params.page) || 1;
-
-  return (
-    <Pagination
-      itemCount={100}
-      pageSize={10}
-      currentPage={page}
-    />
-  );
+export default function Home() {
+  return <div>Hello world </div>;
 }
