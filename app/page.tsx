b7f18bb248fd5810b94e8cf7e0d19d43bfaf205d
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2025-12-24 11:22:01 +0200

    Display issue summary and latest issues on the homepage with a new grid layout.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,27 @@
 import prisma from "@/prisma/client";
-import IssueSummary from "./IssueSummary";
+import { Flex, Grid } from "@radix-ui/themes";
 import IssueChart from "./IssueChart";
+import IssueSummary from "./IssueSummary";
+import LatestIssues from "./LatestIssues";
 
 export default async function Home() {
-  const open = await prisma.issue.count({ where: { status: "OPEN" } });
-  const inPorgress = await prisma.issue.count({ where: { status: "IN_PROGRESS" } });
-  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+  const open = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const inPorgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
 
-  return <IssueChart open={open} inProgress={inPorgress} closed={closed} />;
+  return (
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Flex direction="column" gap="5">
+        <IssueSummary open={open} inProgress={inPorgress} closed={closed} />
+        <IssueChart open={open} inProgress={inPorgress} closed={closed} />
+      </Flex>
+      <LatestIssues />
+    </Grid>
+  );
 }
