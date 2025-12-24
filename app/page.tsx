29cdd11cd70a2452d1a7319be289ca70c228dc4d
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2025-12-24 08:18:42 +0200

    Implemenp Pagination functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <div>Hello world </div>;
+import Pagination from "./components/Pagination";
+
+interface Props {
+  searchParams: { page: string };
+}
+export default async function Home({ searchParams }: Props) {
+  const params = await searchParams;
+  const page = parseInt(params.page) || 1;
+
+  return (
+    <Pagination
+      itemCount={100}
+      pageSize={10}
+      currentPage={page}
+    />
+  );
 }
